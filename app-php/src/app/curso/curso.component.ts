03498fc45c74402612fc566e7468d6b281d0f319
Gilberto Calazans <g.calazans10@gmail.com>
2022-06-12 09:55:59 -0300

    Implementação inicial da Estrotura de Serviço

--- app-php/src/app/curso/curso.component.ts
+++ app-php/src/app/curso/curso.component.ts
@@ -1,15 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Curso } from './curso';
+
 
 @Component({
   selector: 'app-curso',
   templateUrl: './curso.component.html',
   styleUrls: ['./curso.component.css']
 })
+
 export class CursoComponent implements OnInit {
 
-  constructor() { }
+  //URL XAMPP
+  url = "http://localhost/api/php/";
 
-  ngOnInit(): void {
+  //Vetor do Curso
+  vetor: Curso[] | undefined;
+
+  //Construtor
+  constructor (private http:HttpClient){
+
   }
 
+
+// Inicializador
+  ngOnInit(): void {
+    
+  }
+  //Cadastar
+  cadastro():void{
+    alert("CADASTRO");
+  }
+
+  //Seleção
+  selecao():void{
+    alert("SELEÇÃO");
+  }
+
+  //Alterar
+  alterar():void{
+    alert("ALTERAR");
+  }
+
+  //Remover
+  remover():void{
+    alert("REMOVER");
+  }
 }
---
+++ app-php/src/app/curso/curso.ts
@@ -0,0 +1,8 @@
+//Classe
+export class Curso{
+
+    //Construtor
+    construtor(nomeCurso:string, valorCurso:number, idCurso?:number){
+        
+    }
+}
